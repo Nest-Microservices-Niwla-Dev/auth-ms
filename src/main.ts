2c91e6efc03a1a36298d704c77517d9bb6a546c5
Nicolas Ortega <nicolasortegabustos@gmail.com>
2024-10-05 00:35:00 -0300

    update auth-ms

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+ 
+  // port 3004 env
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options:{
+       servers: envs.natsServers
+      }
+   }
+);
+
+  app.useGlobalPipes( 
+    new ValidationPipe({ 
+    whitelist: true, 
+    forbidNonWhitelisted: true, 
+    }) 
+   );
+   
+   await app.listen();
+
 }
 bootstrap();
